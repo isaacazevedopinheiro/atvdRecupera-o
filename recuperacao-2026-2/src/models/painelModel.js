64d113bbd0ddea1cab967552dd07911546d32641
isaacazevedopinheiro <isaac.pinheiro@sptech.school>
2026-07-16 17:50:56 -0300

    parte 2 (so funciona ate a parte 1)

--- recuperacao-2026-2/src/models/painelModel.js
+++ recuperacao-2026-2/src/models/painelModel.js
@@ -1,7 +1,14 @@
 var database = require("../database/config");
 
+function topAutores(precoCompra, precoVenda) {
 
+    var instrucaoSql = `
+          SELECT nome FROM livro where >${precoCompra} LIMIT 3
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
 
 module.exports = {
-
+    topAutores
 }
